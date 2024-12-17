3ba7c3705861e1402c0ac2bacafd1ba7bb1f06eb
ikokywu <rikiridwan949@gmail.com>
2024-12-17 23:20:37 +0700

    feat: add navbar component

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import './index.css';
-import App from './App.jsx';
 import { Provider } from 'react-redux';
-import { persistor, store } from '@store/index';
 import { PersistGate } from 'redux-persist/integration/react';
+
+import { persistor, store } from '@store/index';
+
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
