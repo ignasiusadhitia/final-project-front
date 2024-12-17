d849149c022694ad18b647a471219a2953586d35
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-12-17 17:51:31 +0700

    style(auth): add login form

---
+++ src/components/auth/LogInForm.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const LogInForm = ({ onSubmitHandler }) => {
+  return (
+    <form className="flex flex-col gap-8" onSubmit={onSubmitHandler}>
+      <input
+        className="w-full border-b-2 py-2 leading-6 focus:outline-none"
+        id="email"
+        name="email"
+        placeholder="Email or Phone Number"
+        type="text"
+      />
+      <input
+        className="w-full border-b-2 py-2 leading-6 focus:outline-none"
+        id="password"
+        name="password"
+        placeholder="Password"
+        type="password"
+      />
+
+      <div className="flex flex-col-reverse md:flex-row items-end md:items-center justify-between mt-2">
+        <button
+          className="w-full md:w-fit bg-button-2 hover:bg-button-hover-1 py-4 px-12 rounded text-white"
+          type="submit"
+        >
+          Log In
+        </button>
+
+        <span className="block text-secondary-3 text-base mb-2 md:mb-0 ">
+          <Link>Forget Password?</Link>
+        </span>
+      </div>
+    </form>
+  );
+};
+
+LogInForm.propTypes = {
+  onSubmitHandler: PropTypes.func,
+};
+
+export default LogInForm;
--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Footer } from './Footer';
 export { default as Navbar } from './Navbar';
 export { default as Skeleton } from './Skeleteon';
 export { default as SignUpForm } from './auth/SignUpForm';
+export { default as LogInForm } from './auth/LogInForm';
