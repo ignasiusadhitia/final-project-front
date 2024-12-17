4be9bff7132d1649d591ed0425583c3dce4d240a
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-12-17 14:52:46 +0700

    style: set main component minimum height to screen height

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -5,13 +5,15 @@ import PropTypes from 'prop-types';
 const SectionTitle = ({ title = 'Title', subTitle = 'Sub Title' }) => {
   return (
     <div className="flex flex-col gap-5">
-      <div className="flex gap-4 items-center">
+      <div className="hidden md:flex gap-4 items-center">
         <div className="w-5 h-10 bg-secondary-3 rounded"></div>
-        <h3 className='text-base font-["Poppins"] text-secondary-3 font-semibold'>
+        <h3 className="text-base  text-secondary-3 font-semibold">
           {subTitle}
         </h3>
       </div>
-      <h2 className="text-4xl font-semibold leading-[3rem]">{title}</h2>
+      <h2 className="text-4xl font-semibold leading-[3rem] font-['Inter']">
+        {title}
+      </h2>
     </div>
   );
 };
