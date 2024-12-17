5843d84118ddbc9eb80bf812d131bb9f0226c0d5
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-12-17 17:00:22 +0700

    style(auth): add white background to form

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -5,16 +5,21 @@ import { Link } from 'react-router-dom';
 import { SignUpForm } from '@components';
 
 const SignUp = () => {
+  const submitHandler = (event) => {
+    event.preventDefault();
+    console.log('Submitted');
+  };
+
   return (
-    <div className="h-screen md:bg-auth-banner bg-no-repeat flex mt-6 md:mt-[3.75rem] md:mb-[8.75rem]">
+    <div className="h-screen md:bg-auth-banner bg-no-repeat bg-contain flex mt-6 md:mt-[3.75rem] md:mb-[8.75rem]">
       <div className="container h-full grid grid-cols-1 md:grid-cols-2">
         <div className="hidden md:block"></div>
-        <div className="w-full h-full flex flex-col justify-start md:justify-center my-auto px-6 md:pl-48">
+        <div className="w-full h-full flex flex-col justify-start md:justify-center my-auto px-6 md:pl-48 bg-white">
           <h1 className="text-2xl md:text-4xl font-medium mb-6 font-['Inter']">
             Create an account
           </h1>
           <p className="mb-12 text-xs md:text-base">Enter your details below</p>
-          <SignUpForm />
+          <SignUpForm onSubmitHandler={submitHandler} />
           <div className="flex items-center gap-4 mt-4">
             <span className="block text-xs md:text-base">
               Already have account?
