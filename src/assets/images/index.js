77fd380c4eaf7b261aa7d6bcaa8e206df92b9875
Dede Supriatna <dedesupriatna1421@mail.com>
2024-12-17 13:20:53 +0700

    feat: add footer component

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,1 +1,4 @@
 export { default as articleImg } from './articleImg.webp';
+export { default as playStore } from './google-play-store.png';
+export { default as appleStore } from './apple-store.png';
+export { default as qrCode } from './qr-code.png';
