a3da7458b7300a49df202d07788b2120e8452de6
Dede Supriatna <dedesupriatna1421@mail.com>
2024-12-18 17:02:14 +0700

    feat: add wishlist page

--- src/components/index.js
+++ src/components/index.js
@@ -6,5 +6,7 @@ export { default as SignUpForm } from './auth/SignUpForm';
 export { default as LogInForm } from './auth/LogInForm';
 export { default as SectionTitle } from './SectionTitle';
 export { default as Guarantee } from './Guarantee';
+export { default as ProductGrid } from './ProductGrid';
+export { default as ProductsCard } from './ProductsCard';
 // About page
 export { default as TeamsCard } from './about/TeamsCard';
