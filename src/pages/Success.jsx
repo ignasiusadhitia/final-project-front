63fb99d59be8874b705365a8a47bd127c2b8dd27
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-12-25 15:32:28 +0700

    style(checkout-flow): add success page mobile view

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -23,13 +23,13 @@ const Success = () => {
 
   return (
     <main className="container px-6 md:px-0">
-      <div className="max-w-[27.813rem] mx-auto mt-[4.438rem]">
+      <div className="max-w-[27.813rem] mx-auto mt-6 md:mt-[4.438rem]">
         {order?.products?.length !== 0 ? (
           <>
-            <h1 className="w-full text-center text-4xl font-medium">
+            <h1 className="w-full text-left md:text-center text-base md:text-4xl font-medium">
               Order Success
             </h1>
-            <div className=" flex flex-col mt-[4.563rem] gap-8">
+            <div className=" flex flex-col mt-4 md:mt-[4.563rem] gap-8">
               {order.products.map((item) => (
                 <CheckoutProductCard key={item.id} product={item} />
               ))}
@@ -51,11 +51,13 @@ const Success = () => {
 
             <div className="flex justify-between mt-4">
               <span className="block">Total:</span>
-              <span className="block">${total}</span>
+              <span className="block font-semibold md:font-normal">
+                ${total}
+              </span>
             </div>
 
             <Link
-              className="w-full mt-8 block bg-button-2 hover:bg-button-hover-1 text-text-1 rounded py-4 text-center"
+              className="w-full mt-8 mb-12 md:mb-0 block bg-button-2 hover:bg-button-hover-1 text-text-1 rounded py-4 text-center"
               to="/"
             >
               Back to Home
